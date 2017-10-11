f3b22d717bd0b9f25b79edc83b6af50cc191ead6
Masanori Igarashi <askof7th@gmail.com>
2017-10-11 09:36:13 +0900

    Create 8-4.cpp

---
+++ 8-4.cpp
@@ -0,0 +1,22 @@
+/*
+
+Question: Design a parking lot using object-oriented principles.
+
+How to approach:
+
+1. Handle ambiguity
+2. Define core objects
+3. Analyze relationships
+4. Investigate actions
+
+Assumptions
+- Has several floors
+- For buses, cars and motor cycles
+- Coinparking
+
+Core objects
+- A parking lot
+-- Spaeces for buses, cars and motor cycles
+--- position (floor and #), available, how many hours occupied
+-- Fare adjustment machine
+*/
