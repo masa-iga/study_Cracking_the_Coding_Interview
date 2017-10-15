4df8bd2dd238666c51a1bbfcfc8d828106295ff3
Masanori Igarashi <askof7th@gmail.com>
2017-10-15 21:48:23 +0900

    Create 8-5.cpp

---
+++ 8-5.cpp
@@ -0,0 +1,22 @@
+/*
+  Question: Design the data structures for an online book reader system
+
+  How to approcah:
+    1. Handle ambiguity
+    2. Define core objects
+    3. Analyze relationships
+    4. Investigate actions
+*/
+
+/*
+  Assumptions:
+    - Assume kidle online reader
+    - A user which has an account can access his personal ebook
+    - Generally, two users (accounts) access the ebook simultaneously
+    - The service is available for several OSs. e.g. Windows, Mac, Android and iOS
+*/
+
+/*
+  Core objects:
+    - 
+*/
