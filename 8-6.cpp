03af0379c6b45b485e0a79e612e3b9415b62ca51
Masanori Igarashi <askof7th@gmail.com>
2017-10-17 07:49:51 +0900

    Create 8-6.cpp

---
+++ 8-6.cpp
@@ -0,0 +1,12 @@
+/*
+  Question:
+    Implement a jigsaw puzzle. Design the data structures and explain an algorithm to solve the puzzle. You can assume that you have a fitsWith method which, when passed two puzzle pieces, returns true if the two pieces belong together.
+*/
+
+/*
+  How to approach:
+    1. Handle ambiguity
+    2. Define core objects
+    3. Analyze relationships
+    4. Investigate actions
+*/
