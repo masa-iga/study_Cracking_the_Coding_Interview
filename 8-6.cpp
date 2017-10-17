376b3a456d309a417a9f3a8c74b4acb8b10cd69a
Masanori Igarashi <askof7th@gmail.com>
2017-10-17 22:44:56 +0900

    Update 8-6.cpp

--- 8-6.cpp
+++ 8-6.cpp
@@ -14,7 +14,42 @@
 */
 
 /*
-コードを書くにあたって課題:
-  今のパズルの状態（どのピースがはまっていて、どのピースははまっていない）をどうやって表現するか？？
-    ピースにハマッているか、まだハマッていないか、のステートをPiece classに持たせる
+
+@startuml
+
+class JigsawPuzzle {
+    - Piece[][] pieces
+    - const uint32_t numOfWidth
+    - const uint32_t numOfHeight
+    
+    + bool fitsWith(Piece* a, Piece* b)
+    + void generatePuzzle()
+}
+
+class Piece {
+    - Point correctPos
+    - Edge leftEdge
+    - Edge upperEdge
+    - Edge rightEdge
+    - Edge lowerEdge
+    - Texture texture
+    - Point currentPos
+    - int currentAngle
+    - bool fitted
+    
+    + Piece(Point correctPos, Edge l, Edge u, Edge r, Edge l)
+    + bool setTexture(Texture tex)
+    + bool setCurrentPos(Point p)
+    + bool setAngle(int angle)
+    + bool isFitted()
+}
+
+enum Edge {
+    dint, convex, edge
+}
+
+JigsawPuzzle *-- Piece
+
+@enduml
+
 */
