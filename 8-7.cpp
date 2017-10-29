42bdd309d8af11f0e6bf1972727718da1d4d5010
Masanori Igarashi <askof7th@gmail.com>
2017-10-30 07:51:19 +0900

    Create 8-7.cpp

---
+++ 8-7.cpp
@@ -0,0 +1,19 @@
+/*
+  Question:
+    Explain how you would design a chat server. In partjcular, provide detail about the various backend components, classes, and methods. What would be the hardest problem to solve?
+
+  How to approach:
+    1. Handle ambiguity
+    2. Define core classes
+    3. Analyize relashinships
+    4. Investigate actions
+*/
+
+/*
+  Let's make assumption that:
+    - Imagine Skype
+    -- Need a client software
+    -- Access via account
+    -- Friend list
+    -- Can chat not only with on-by-one, but also multiple persons at the same time
+*/
