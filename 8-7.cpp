608f488f5f8dfcd0faed7933ce9241e5624bb4c1
Masanori Igarashi <askof7th@gmail.com>
2017-10-30 13:56:49 +0900

    Update 8-7.cpp

--- 8-7.cpp
+++ 8-7.cpp
@@ -15,5 +15,26 @@
     -- Need a client software
     -- Login via account
     -- Friend/contact list
-    -- Can chat not only with on-by-one, but also multiple persons at the same time
+    -- Suppots not only one-on-one chat (private), but also group chats
+
+  Core classes
+    - Server
+    - View
+    - Controller
+*/
+
+/*
+  [Answer]
+  What specific actions does it need to support?
+    - Signing online and offline
+    - Add requests (sending, accepting, and rejecting)
+    - Updating a status message
+    - Creating private and group chats
+    - Adding new messages to private and group chats
+    
+  What are the core components of the system?
+    a database, a set of clients, and a set of servers, but we won't include these parts in our OOD here.
+    
+  What are the key objects and methods?
+    a concept of users, conversations, and status messages.
 */
