8f11ddd82d412f1cf8f28dc0b31ab3d881aac5a2
Masanori Igarashi <askof7th@gmail.com>
2017-11-05 07:31:51 +0900

    Update 8-8.cpp

--- 8-8.cpp
+++ 8-8.cpp
@@ -2,7 +2,7 @@
   Question:
     Othello is played as follows: Each Othello piece is white on one side and black on the
     other. When a piece is surrounded by its opponents on both the left and right sides,
-    or both the top and bottom, it is said to be capture and its color is flipped. On your
+    or both the top and bottom, it is said to be captured and its color is flipped. On your
     turn, you must capture at least one of your opponent's pieces. The game ends when
     either user has no more valid moves. The win is assigned to the person with the most
     pieces. Implement the object-oriented design for Othello.
@@ -31,5 +31,6 @@
     - find if there are valid moves or not
     - Place a piece at a point
     - Flip a piece which is surrounded by the opponent
-    - Count pieces
+    - Turn to the opponent
+    - If the game ends, count valid pieces
 */
