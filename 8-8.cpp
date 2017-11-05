ba06e1c3a6f0a80cc5443cdd1124e100dec5d73b
Masanori Igarashi <askof7th@gmail.com>
2017-11-05 09:04:30 +0900

    Update 8-8.cpp

--- 8-8.cpp
+++ 8-8.cpp
@@ -34,3 +34,89 @@
     - Turn to the opponent
     - If the game ends, count valid pieces
 */
+
+/*
+class Game {
+    Board : board
+    State : currentTurn
+    
+    void : initialize()
+    void : processInput(Player)
+}
+
+class Board {
+    map<Point, Piece> : pieces
+    
+    bool : haveValidMove(State)
+    void : place(Piece, Point)
+}
+
+class Piece {
+    State : state
+    
+    void : flip()
+}
+
+class Point {
+    Point(int x, int y)
+    
+    int : x
+    int : y
+}
+
+Enum State {
+    White, Black
+}
+
+Game *-- Board
+Board o-- "*" Piece
+Board *-- "*" Point
+*/
+
+/*
+Answer:
+  Should BlackPiece and WhitePiece be classes?
+    Each piece may flip back and force between colors frequently, so continuously destroying and creating
+    what is really the same object is propably not wise. It may be better to just
+    have a Piece class, with a flag in it representing the current color.
+
+
+Enum Direction {
+    left, right, up, down
+}
+
+Enum Color {
+    White, Black
+}
+
+class Game {
+    Player[] players
+    {static} Game instance
+    Board board
+    
+    Game()
+    {static} Game getInstance()
+    Board getBoard()
+}
+
+class Board {
+    Piece[][] board
+    
+    Board(int rows, int columns)
+    void : initialize()
+    bool : placeColor(int row, int column, Color color)
+    int : flipSection(int row, int column, Color color, Direction d)
+    int : getScoreForColor(Color c)
+    void : updateScore(Color newColor, int newPieces) 
+}
+
+class Piece {
+}
+
+class Player {
+}
+
+Game *-- Board
+Game *-- "*" Player
+
+*/
