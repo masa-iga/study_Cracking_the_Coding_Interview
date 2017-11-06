e32954cf47860ddce8e3b73a05b71eb1ec7b8023
Masanori Igarashi <askof7th@gmail.com>
2017-11-06 23:05:22 +0900

    Create 8-9.cpp

---
+++ 8-9.cpp
@@ -0,0 +1,36 @@
+/*
+  Question:
+    Explain the data structures and algorithm that you would use to design an
+    in-memory file system. Illustrate with an example in code where possible.
+ 
+  Approach:
+    1. Handle ambiguity
+    2. Define core objects
+    3. Analyize relationships
+    4. Investigate actions
+ */
+ 
+ 
+ 
+ /*
+ 
+ abstract class Entry {
+    # Directory : parent
+    # long : created
+    # long : lastUpdated
+    # long : lastAccessed
+    # string : name
+    
+    Entry(string, Directory)
+}
+
+class File {
+}
+
+class Directory {
+}
+
+Entry <|-- File
+Entry <|-- Directory
+
+*/
