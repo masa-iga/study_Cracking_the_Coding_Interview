f7cadea357635f150e684290db4b5bef02a30c65
Masanori Igarashi <askof7th@gmail.com>
2017-11-07 09:40:25 +0900

    Create 8-10.cpp

---
+++ 8-10.cpp
@@ -0,0 +1,19 @@
+/*
+  Question:
+    Design and implement a hash table which uses chaining (linked lists) to handle collisions.
+  
+  Approach:
+    1. Handle ambiguity
+    2. Define core objects
+    3. Analyize relationships
+    4. Investigate actions
+*/
+
+
+
+/*
+  Collision resolution in hash table
+    1. Separate chaining
+    2. Open addressing
+    3. ...
+*/    
