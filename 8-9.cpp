11c15de388f8d6744f7ef185a4af0e164f491b07
Masanori Igarashi <askof7th@gmail.com>
2017-11-07 09:23:14 +0900

    Update 8-9.cpp

--- 8-9.cpp
+++ 8-9.cpp
@@ -34,3 +34,30 @@ Entry <|-- File
 Entry <|-- Directory
 
 */
+
+int Directory::size() {
+  int size = 0;
+  for (Entry e : contents) {
+    size += e.size();
+  }
+}
+
+int Directory::numberOfFiles() {
+  int count = 0;
+  for (Entry e : contents) {
+    if (e instanceof Directory) {
+      count++;
+      Directory d = (Directory) e;
+      count += d.numberOfFiles();
+    } else if (e instanceof File) {
+      count++;
+    }
+  }
+  return count;
+}
+
+/*
+  Alternatively, we could have implemented Directory such that it contains separate list for files and subdirectories.
+  This makes the numberOfFiles() method a bit cleaner, since it doesn't need to use the instanceof operator,
+  but it does prohitit us from cleanly sorting files and directories by dates or names.
+*/
