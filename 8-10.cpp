03440aca0c6b5dc018a955e74e85e5ffc1ec3566
Masanori Igarashi <askof7th@gmail.com>
2017-11-13 09:32:29 +0900

    8-10: sanity test passes

--- 8-10.cpp
+++ 8-10.cpp
@@ -27,6 +27,8 @@
     int a = hash[key];
 */
 
+//#define TEST_CHAINING (1)
+
 #include <cstdio>
 #include <cstdint>
 #include <string>
@@ -39,7 +41,13 @@ using namespace std;
 
 struct Node {
   Node* next_ = NULL;
-  string d_;
+  string key_;
+  string val_;
+  Node() {};
+  Node(string key, string val)
+  : key_(key),
+    val_(val)
+  { };
 };
 
 class HashTable {
@@ -48,12 +56,17 @@ public:
   : size_(size)
   {
     CHECK(size > 0);
-#if 0
-    table_ = new string[size];
-#endif
     table_ = new Node*[size];
   }
   ~HashTable() {
+    for (uint32_t i = 0; i < size_; i++) {
+      Node *node = table_[i];
+      while (node != NULL) {
+        Node *next = node->next_;
+        delete node;
+        node = next;
+      }
+    }
     delete[] table_;
   }
 
@@ -63,9 +76,6 @@ public:
 
 private:
   const uint32_t size_;
-#if 0
-  string *table_;
-#endif
   Node **table_;
   uint32_t hashFunction(string key, uint32_t max);
 };
@@ -73,34 +83,46 @@ private:
 void HashTable::set(string key, string val) {
   const uint32_t idx = hashFunction(key, size_);
   CHECK(0 <= idx && idx < size_);
-#if 0
-  table_[idx] = val;
-#endif
+  printf("%s : table_[%d] = %p\n",
+    __func__, idx, table_[idx]);
+  if (table_[idx] == NULL) {
+    table_[idx] = new Node(key, val);
+    printf("%s : allocate Node(%s, %s) @ %p\n",
+      __func__, key.c_str(), val.c_str(), table_[idx]);
+    return ;
+  }
   Node *pNode = table_[idx];
-  while (pNode != NULL) {
+  while (pNode->next_ != NULL) {
     pNode = pNode->next_;
   }
+  pNode->next_ = new Node(key, val);
+  cout << __func__ << " : pNode = " << pNode << endl;
+  cout << __func__ << " : (key, val) = (" << key << ", " << val << ")" << endl;
 }
 
 string HashTable::get(string key) {
   const uint32_t idx = hashFunction(key, size_);
-#if 0
-  return table_[idx];
-#endif
+  cout << __func__ << " : idx = " << idx << endl;
   Node *pNode = table_[idx];
-  while (pNode != NULL && pNode->d_ != key) {
+  while (pNode != NULL && pNode->key_ != key) {
+    cout << __func__ << " : (key, val) = (" << pNode->key_ << ", " << pNode->val_ << ")" << endl;
     pNode = pNode->next_;
   }
+  cout << __func__ << " : pNode = " << pNode << endl;
   const string none = "";
-  if (pNode != NULL)
-    return table_[idx]->d_;
-  else
+  if (pNode == NULL)
     return none;
+  else
+    return pNode->val_;
 }
 
 uint32_t HashTable::hashFunction(string key, uint32_t max) {
+#if TEST_CHAINING
+  return 1;
+#else
   hash<string> str_hash;
   return ((uint32_t)str_hash(key)) % max;
+#endif //TEST_CHAINING
 }
 
 /*******************************
@@ -122,21 +144,21 @@ TEST_GROUP(FirstTestGroup)
 TEST(FirstTestGroup, GeneralPattern) {
   cout << "---   FirstTestGroup   ---" << endl;
 
-  const string key0("key1");
-  const string exp0("val1");
-  hashTable->set(key0, exp0);
-  STRCMP_EQUAL(exp0.c_str(), hashTable->get(key0).c_str());
+  const string key0("key0");
+  const string val0("val0");
+  hashTable->set(key0, val0);
+  STRCMP_EQUAL(val0.c_str(), hashTable->get(key0).c_str());
 
-  const string key1("123bs");
-  const string exp1("2323");
-  hashTable->set(key1, exp1);
-  STRCMP_EQUAL(exp1.c_str(), hashTable->get(key1).c_str());
+  const string key1("key1");
+  const string val1("val1");
+  hashTable->set(key1, val1);
+  STRCMP_EQUAL(val1.c_str(), hashTable->get(key1).c_str());
 
-  const string key2(key0);
-  const string exp2("aAz");
-  hashTable->set(key2, exp2);
-  STRCMP_EQUAL(exp2.c_str(), hashTable->get(key2).c_str());
-  STRCMP_EQUAL(exp0.c_str(), hashTable->get(key0).c_str());
+  const string key2("key2");
+  const string val2("val2");
+  hashTable->set(key2, val2);
+  STRCMP_EQUAL(val2.c_str(), hashTable->get(key2).c_str());
+  STRCMP_EQUAL(val0.c_str(), hashTable->get(key0).c_str());
 }
 
 int main(int argc, char** argv)
