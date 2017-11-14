4cfd38c2c0bbcea9ac4de88800211ba43352dda0
Masanori Igarashi <askof7th@gmail.com>
2017-11-14 09:57:17 +0900

    8-10 : Generaize with template class

--- 8-10.cpp
+++ 8-10.cpp
@@ -20,13 +20,6 @@
 
 
 
-/*
-  Interface:
-    map<string, int> hash;
-    hash[key] = 10;
-    int a = hash[key];
-*/
-
 //#define TEST_CHAINING (1)
 
 #include <cstdio>
@@ -39,89 +32,108 @@
 using namespace std;
 
 
+template <class T, class U>
 struct Node {
   Node* next_ = NULL;
-  string key_;
-  string val_;
+  T key_;
+  U val_;
   Node() {};
-  Node(string key, string val)
+  Node(T key, U val)
   : key_(key),
     val_(val)
   { };
 };
 
+template <class T, class U>
 class HashTable {
 public:
-  HashTable(uint32_t size)
-  : size_(size)
-  {
-    CHECK(size > 0);
-    table_ = new Node*[size];
-  }
-  ~HashTable() {
-    for (uint32_t i = 0; i < size_; i++) {
-      Node *node = table_[i];
-      while (node != NULL) {
-        Node *next = node->next_;
-        delete node;
-        node = next;
-      }
-    }
-    delete[] table_;
-  }
+  HashTable(uint32_t size);
+  ~HashTable();
 
-  void set(string key, string val);
-  string get(string key);
-  void remove(string key);
+  void set(T key, U val);
+  U get(T key);
+  void remove(T key);
 
 private:
   const uint32_t size_;
-  Node **table_;
-  uint32_t hashFunction(string key, uint32_t max);
+  Node<T, U> **table_;
+  uint32_t hashFunction(T key, uint32_t max);
 };
 
-void HashTable::set(string key, string val) {
+
+template <class T, class U>
+HashTable<T, U>::HashTable(uint32_t size)
+: size_(size)
+{
+    CHECK(size > 0);
+    table_ = new Node<T, U>*[size];
+}
+
+template <class T, class U>
+HashTable<T, U>::~HashTable() {
+  for (uint32_t i = 0; i < size_; i++) {
+    Node<T, U> *node = table_[i];
+    while (node != NULL) {
+      Node<T, U> *next = node->next_;
+      delete node;
+      node = next;
+    }
+  }
+  delete[] table_;
+}
+
+template <class T, class U>
+void HashTable<T, U>::set(T key, U val) {
   const uint32_t idx = hashFunction(key, size_);
   CHECK(0 <= idx && idx < size_);
   printf("%s : table_[%d] = %p\n",
     __func__, idx, table_[idx]);
   if (table_[idx] == NULL) {
-    table_[idx] = new Node(key, val);
-    printf("%s : allocate Node(%s, %s) @ %p\n",
-      __func__, key.c_str(), val.c_str(), table_[idx]);
+    table_[idx] = new Node<T, U>(key, val);
     return ;
   }
-  Node *pNode = table_[idx];
+  Node<T, U> *pNode = table_[idx];
   while (pNode->next_ != NULL) {
     pNode = pNode->next_;
   }
-  pNode->next_ = new Node(key, val);
-  cout << __func__ << " : pNode = " << pNode << endl;
+  pNode->next_ = new Node<T, U>(key, val);
+  cout << __func__ << " : pNode addr = " << pNode << endl;
   cout << __func__ << " : (key, val) = (" << key << ", " << val << ")" << endl;
 }
 
-string HashTable::get(string key) {
+template <class T, class U>
+U HashTable<T, U>::get(T key) {
   const uint32_t idx = hashFunction(key, size_);
   cout << __func__ << " : idx = " << idx << endl;
-  Node *pNode = table_[idx];
+  Node<T, U> *pNode = table_[idx];
   while (pNode != NULL && pNode->key_ != key) {
     cout << __func__ << " : (key, val) = (" << pNode->key_ << ", " << pNode->val_ << ")" << endl;
     pNode = pNode->next_;
   }
-  cout << __func__ << " : pNode = " << pNode << endl;
-  const string none = "";
-  if (pNode == NULL)
-    return none;
-  else
+  cout << __func__ << " : pNode addr = " << pNode << endl;
+
+  if (pNode != NULL)
     return pNode->val_;
+
+  cout << __func__
+    << " : Warn that cannot find the corresponding value from hash table (key = "
+    << key << ")" << endl;
+
+  return NULL;
 }
 
-uint32_t HashTable::hashFunction(string key, uint32_t max) {
+template <class T, class U>
+void HashTable<T, U>::remove(T key) {
+  ;
+}
+
+template <class T, class U>
+uint32_t HashTable<T, U>::hashFunction(T key, uint32_t max) {
 #if TEST_CHAINING
   return 1;
 #else
-  hash<string> str_hash;
-  return ((uint32_t)str_hash(key)) % max;
+  hash<T> hash;
+  return ((uint32_t)hash(key)) % max;
 #endif //TEST_CHAINING
 }
 
@@ -131,10 +143,10 @@ uint32_t HashTable::hashFunction(string key, uint32_t max) {
 
 TEST_GROUP(FirstTestGroup)
 {
-    HashTable *hashTable;
+    HashTable<string, string> *hashTable;
     const uint32_t size = 1000;
     TEST_SETUP() {
-        hashTable = new HashTable(size);
+        hashTable = new HashTable<string, string>(size);
     }
     TEST_TEARDOWN() {
         delete hashTable;
