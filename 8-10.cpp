8fac5e7f1c1d9b64a13deb622da390a7420e1491
Masanori Igarashi <askof7th@gmail.com>
2017-11-15 09:16:31 +0900

    8-10 : Refine

--- 8-10.cpp
+++ 8-10.cpp
@@ -38,11 +38,30 @@ struct Node {
   T key_;
   U val_;
   Node() {};
-  Node(T key, U val)
-  : key_(key),
-    val_(val)
-  { };
+  Node(T key, U val);
+  bool equivalent(Node<T, U> node);
+  bool equivalent(T key);
 };
+
+template <class T, class U>
+Node<T, U>::Node(T key, U val)
+: key_(key),
+  val_(val)
+{
+  cout << __func__ << " : New node created. ("
+    << key << ", " << val << ") @ " << this
+    << endl;
+}
+
+template <class T, class U>
+bool Node<T, U>::equivalent(Node<T, U> node) {
+  return (node.key == key_);
+}
+
+template <class T, class U>
+bool Node<T, U>::equivalent(T key) {
+  return (key == key_);
+}
 
 template <class T, class U>
 class HashTable {
@@ -50,14 +69,14 @@ public:
   HashTable(uint32_t size);
   ~HashTable();
 
-  void set(T key, U val);
+  void put(T key, U val);
   U get(T key);
   void remove(T key);
 
 private:
   const uint32_t size_;
   Node<T, U> **table_;
-  uint32_t hashFunction(T key, uint32_t max);
+  uint32_t hashFunc(T key, uint32_t max);
 };
 
 
@@ -83,34 +102,37 @@ HashTable<T, U>::~HashTable() {
 }
 
 template <class T, class U>
-void HashTable<T, U>::set(T key, U val) {
-  const uint32_t idx = hashFunction(key, size_);
+void HashTable<T, U>::put(T key, U val) {
+  const uint32_t idx = hashFunc(key, size_);
   CHECK(0 <= idx && idx < size_);
-  printf("%s : table_[%d] = %p\n",
-    __func__, idx, table_[idx]);
+  printf("%s : table_[%d] = %p\n", __func__, idx, table_[idx]);
   if (table_[idx] == NULL) {
     table_[idx] = new Node<T, U>(key, val);
     return ;
   }
+  /* Look for items with same key and replace if found.
+   * Add the key if not found */
   Node<T, U> *pNode = table_[idx];
-  while (pNode->next_ != NULL) {
+  while (pNode->equivalent(key) || pNode->next_ != NULL) {
     pNode = pNode->next_;
   }
+  if (pNode->equivalent(key)) {
+    pNode->val_ = val;
+    // Replace, or remove and add it at the tail of the linked list
+    return ;
+  }
+
   pNode->next_ = new Node<T, U>(key, val);
-  cout << __func__ << " : pNode addr = " << pNode << endl;
-  cout << __func__ << " : (key, val) = (" << key << ", " << val << ")" << endl;
 }
 
 template <class T, class U>
 U HashTable<T, U>::get(T key) {
-  const uint32_t idx = hashFunction(key, size_);
-  cout << __func__ << " : idx = " << idx << endl;
+  const uint32_t idx = hashFunc(key, size_);
   Node<T, U> *pNode = table_[idx];
-  while (pNode != NULL && pNode->key_ != key) {
-    cout << __func__ << " : (key, val) = (" << pNode->key_ << ", " << pNode->val_ << ")" << endl;
+  cout << __func__ << " : idx = " << idx << endl;
+  while (pNode != NULL && !pNode->equivalent(key)) {
     pNode = pNode->next_;
   }
-  cout << __func__ << " : pNode addr = " << pNode << endl;
 
   if (pNode != NULL)
     return pNode->val_;
@@ -128,7 +150,7 @@ void HashTable<T, U>::remove(T key) {
 }
 
 template <class T, class U>
-uint32_t HashTable<T, U>::hashFunction(T key, uint32_t max) {
+uint32_t HashTable<T, U>::hashFunc(T key, uint32_t max) {
 #if TEST_CHAINING
   return 1;
 #else
@@ -158,17 +180,17 @@ TEST(FirstTestGroup, GeneralPattern) {
 
   const string key0("key0");
   const string val0("val0");
-  hashTable->set(key0, val0);
+  hashTable->put(key0, val0);
   STRCMP_EQUAL(val0.c_str(), hashTable->get(key0).c_str());
 
   const string key1("key1");
   const string val1("val1");
-  hashTable->set(key1, val1);
+  hashTable->put(key1, val1);
   STRCMP_EQUAL(val1.c_str(), hashTable->get(key1).c_str());
 
   const string key2("key2");
   const string val2("val2");
-  hashTable->set(key2, val2);
+  hashTable->put(key2, val2);
   STRCMP_EQUAL(val2.c_str(), hashTable->get(key2).c_str());
   STRCMP_EQUAL(val0.c_str(), hashTable->get(key0).c_str());
 }
